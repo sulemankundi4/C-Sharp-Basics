e218dc86084f82184a0e79d2ccb888f49db621d5
Muhammad Suleman <147092019+sulemankundi4@users.noreply.github.com>
2024-10-15 07:03:21 +0500

    Getting hands dirty with the Basics!

--- firstProgram/Program.cs
+++ firstProgram/Program.cs
@@ -2,3 +2,28 @@
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World This is my first program");
+
+
+string firstName = "Muhammad";
+string lastName = "Suleman";
+
+
+int age = 25;
+
+age = 30;
+
+
+Console.WriteLine("Hello, " + firstName + " " + lastName);
+Console.WriteLine("Hello, " + firstName + " " + lastName + " " + age);
+
+int add = 5+10;
+int sub = 20-10;
+int mul = 5*10;
+int div = 20/12;
+
+// template literals something like this!
+Console.WriteLine($"addition: {add}");
+
+float res = 20/12f ;
+
+Console.WriteLine($"division: {res}");
